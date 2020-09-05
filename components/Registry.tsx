82dae2b14d7b4a617d10621d8f1dd469776020f9
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-05 23:45:02 +0500

    Update Registry.tsx

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -369,9 +369,9 @@ const Registry = () => {
                             <div className="w-full p-4">
                               <div className="w-4/5 sm:w-1/3 bg-gray-100 h-8"/>
                               <div className="sm:w-2/3 bg-gray-100 h-3 mt-6"/>
-                              <div className="w-5/6 sm:w-3/4 bg-gray-100 h-3 mt-4"></div>
-                              <div className="sm:w-3/5 bg-gray-100 h-3 mt-4"></div>
-                              <div className="w-3/4 bg-gray-100 h-3 mt-4"></div>
+                              <div className="w-5/6 sm:w-3/4 bg-gray-100 h-3 mt-4"/>
+                              <div className="sm:w-3/5 bg-gray-100 h-3 mt-4"/>
+                              <div className="w-3/4 bg-gray-100 h-3 mt-4"/>
                               <div className="sm:w-2/3 bg-gray-100 h-3 mt-4"></div>
                               <div className="w-2/4 sm:w-3/5 bg-gray-100 h-3 mt-4"></div>
                             </div>
