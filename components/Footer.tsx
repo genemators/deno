eecb5c63dcb5daf73fbbff892e8914bf6bcbc8ce
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-05 22:23:09 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,23 +39,16 @@ const Footer = (props: { simple?: boolean }) => (
           </Link>
         </div>
         <div className="px-5 py-2">
-          <Link href="/benchmarks">
-            <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              Benchmarks
-            </a>
-          </Link>
-        </div>
-        <div className="px-5 py-2">
           <Link href="/artwork">
             <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              Artwork
+              Tasvirlar
             </a>
           </Link>
         </div>
         <div className="px-5 py-2">
           <Link href="/posts">
             <a className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              News
+              Xabarlar
             </a>
           </Link>
         </div>
